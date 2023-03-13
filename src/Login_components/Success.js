26f3cd1f5dc81ecec5757db83508ec2c7edc29ce
Gayugai3 <gayathri.tn.1994@gmail.com>
2023-03-13 18:54:31 +0530

    size updated

--- src/Login_components/Success.js
+++ src/Login_components/Success.js
@@ -10,7 +10,10 @@ function Success() {
         <div class="col-lg-12 ">
           <br />
           <br /> <h2 style={{ color: "#0fad00" }}>Success</h2>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc03-AXYKcBGvYB_0xtgw9wBVQmHy0QLOOs_GhFNoBWg&usqp=CAU&ec=48600112" />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc03-AXYKcBGvYB_0xtgw9wBVQmHy0QLOOs_GhFNoBWg&usqp=CAU&ec=48600112"
+            style={{ height: "200", width: "200" }}
+          />
           <h3>{`Dear ${username}`}</h3>
           <p style={{ fontSize: "20px", color: "#5C5C5C" }}>
             Thank you for using our website
