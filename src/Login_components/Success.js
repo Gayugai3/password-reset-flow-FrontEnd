d8041548156f9edf359b14d5f819f1f7a5fb7a2b
Gayugai3 <gayathri.tn.1994@gmail.com>
2023-03-13 18:49:01 +0530

    success page updated

--- src/Login_components/Success.js
+++ src/Login_components/Success.js
@@ -3,14 +3,15 @@ import UserContext from "../context/UserContext";
 
 function Success() {
   const userContextData = useContext(UserContext);
+  const username = localStorage.getItem("userName");
   return (
     <div class="container">
       <div class="row text-center">
-        <div class="col-sm-6 col-sm-offset-3">
+        <div class="col-lg-12 ">
           <br />
           <br /> <h2 style={{ color: "#0fad00" }}>Success</h2>
           <img src="http://osmhotels.com//assets/check-true.jpg" />
-          <h3>Dear, Faisal khan</h3>
+          <h3>{`Dear ${username}`}</h3>
           <p style={{ fontSize: "20px", color: "#5C5C5C" }}>
             Thank you for using our website
           </p>
