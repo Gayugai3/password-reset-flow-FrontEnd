36bdbb1175d72e3fbb0b5f300a03f68dd3ea0942
Gayugai3 <gayathri.tn.1994@gmail.com>
2023-03-13 18:44:27 +0530

    redirect file added

--- src/Login_components/Success.js
+++ src/Login_components/Success.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 function Success() {
+  const userContextData = useContext(UserContext);
   return (
-    <div>Success</div>
-  )
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-6 col-sm-offset-3">
+          <br />
+          <br /> <h2 style={{ color: "#0fad00" }}>Success</h2>
+          <img src="http://osmhotels.com//assets/check-true.jpg" />
+          <h3>Dear, Faisal khan</h3>
+          <p style={{ fontSize: "20px", color: "#5C5C5C" }}>
+            Thank you for using our website
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Success
+export default Success;
